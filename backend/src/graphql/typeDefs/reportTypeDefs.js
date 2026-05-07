a8e86910983281db0517a4431351497dd1b6eabf
adeptpro <77738834+ennytlos@users.noreply.github.com>
2026-05-07 19:08:31 +0100

    Delete backend directory

--- backend/src/graphql/typeDefs/reportTypeDefs.js
+++
@@ -1,9 +0,0 @@
-import { gql } from "apollo-server-express";
-
-const reportTypeDefs = gql`
-  extend type Query {
-    exportLeadsCSV(noticeId: ID!): String!
-  }
-`;
-
-export default reportTypeDefs;
